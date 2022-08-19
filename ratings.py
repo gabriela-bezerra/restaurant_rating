ab74409c31c7589cc2ef28e8dc4c0c36025b75f8
Gabriela <gabrielaa.bdasilva@gmail.com>
2022-08-18 20:10:11 -0700

    Dictionary created

--- ratings.py
+++ ratings.py
@@ -2,3 +2,25 @@
 
 
 # put your code here
+
+def restaurant_ratings(filename):
+
+    data_score = open(filename)
+
+    ratings_log = {}
+
+    for line in data_score:
+        line = line.rstrip()
+        words = line.split(":")
+        restaurant = words[0]
+        score = words[1]
+
+        ratings_log[restaurant] = score
+
+    return ratings_log
+
+    # for word in words:
+    #     ratings_log[word] =
+
+
+print(restaurant_ratings('scores.txt'))
